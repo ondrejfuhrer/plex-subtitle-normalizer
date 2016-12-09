1dc9105b48292a32506df4e7e6bb2247e4b84656
Ondrej Fuhrer <ondrejfuhrer@users.noreply.github.com>
2016-12-09 10:27:09 +0100

    Adding settings (library root)

--- install.sh
+++ install.sh
@@ -6,3 +6,10 @@ then
 fi
 
 chmod +x "$TARGET_FILE"
+
+SETTINGS_FILE=".settings"
+if [ ! -e "$SETTINGS_FILE" ]
+then
+	read -p "Please enter your library directory (script cannot be used outside): " d
+	echo "library_root: $d" > "$SETTINGS_FILE"
+fi
